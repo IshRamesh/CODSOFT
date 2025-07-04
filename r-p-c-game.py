47e7bf1befc697cc2f15145b1f963b11c7c38b78
IshRamesh <ishramesh004@gmail.com>
2025-07-04 22:09:23 +0530

    r-c-p-game is created

---
+++ r-p-c-game.py
@@ -0,0 +1,37 @@
+import random
+
+def get_computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
+
+def get_user_choice():
+    choice = input("Enter your choice (rock/paper/scissors): ").lower()
+    while choice not in ["rock", "paper", "scissors"]:
+        choice = input("Invalid input. Please choose rock, paper, or scissors: ").lower()
+    return choice
+
+def determine_winner(user, computer):
+    if user == computer:
+        return "It's a tie!"
+    elif (user == "rock" and computer == "scissors") or \
+         (user == "paper" and computer == "rock") or \
+         (user == "scissors" and computer == "paper"):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+# Main Game Loop
+while True:
+    print("\n--- Rock Paper Scissors Game ---")
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+
+    print(f"You chose: {user_choice}")
+    print(f"Computer chose: {computer_choice}")
+
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thanks for playing!")
+        break
